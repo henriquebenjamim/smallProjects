d91fb22dc6d569093f94b74d64d4a21c5dcfdce4
henriquebenjamim <henriiqqq@gmail.com>
2022-03-01 18:05:23 -0300

    adding a new small proj to test repo

---
+++ csharp/toDoListMarket/toDoListMarket/toDoListMarket/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace toDoListMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
